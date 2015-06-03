26ec7ff1dfa8011ee69b168dad88156db1ef7702
Valik Grinevich <valik.v1per@gmail.com>
2015-06-03 20:35:32 +0300

    create serials controller and remove default controller

---
+++ src/AppBundle/Controller/SerialsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class SerialsController extends Controller
+{
+    /**
+     * @Template
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $serials = $em->getRepository('AppBundle:Serial')->findAll();
+
+        return [
+            'pagination' => $serials
+        ];
+    }
+
+    /**
+     * @param $slug
+     * @return mixed
+     *
+     * @Template
+     */
+    public function viewAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $serial = $em->getRepository('AppBundle:Serial')->findOneBySlug($slug);
+
+        return $serial;
+    }
+}
